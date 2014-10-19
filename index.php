eef59109d5f622356874afa958459b0c5a440e75
myparrotsteeth <jamiemactulloch@gmail.com>
2014-10-19 22:47:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 	<body>
 		<h2><?php echo "Hello world.." ?></h2>
 		<a href="test.php">Link to second page!</a>
-		<p><?php echo "Words."?></p>
+		<p><?php echo "More words!"?></p>
 	</body>
 </html>
