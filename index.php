38617e628f56ba190dbf7d8758248746dfd548c1
myparrotsteeth <jamiemactulloch@gmail.com>
2014-10-19 22:01:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 	<body>
 		<h2><?php echo "Hello world.." ?></h2>
 		<a href="test.php">Link to second page!</a>
-		<p><?php echo "Please work!"?></p>
+		<p><?php echo `uname r`?></p>
 	</body>
 </html>
